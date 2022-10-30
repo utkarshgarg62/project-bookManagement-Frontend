8d62d2435d2e7adb2d45ae98be04e57540e6aa36
utkarshgarg62 <utkarshgarg143@gmail.com>
2022-10-30 18:30:28 +0530

    book-backend

---
+++ src/models/bookModel.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const objectId = mongoose.Schema.Types.ObjectId
+
+const bookSchema = new mongoose.Schema({
+    cover:{
+        type: String,
+    },
+    title: {
+        type: String,
+        required: true,
+        unqiue: true,
+        trim: true
+    },
+    excerpt: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    userId: {
+        type: objectId,
+        ref: "userModel",
+        required: true,
+        trim: true
+    },
+    ISBN: {
+        type: String,
+        required: true,
+        unqiue: true, trim: true
+    },
+    bookImage:{
+        type:String,
+        // required:true
+    },
+    category: {
+        type: String,
+        required: true, trim: true
+    },
+    subcategory: [{
+        type: String,
+        required: true, trim: true
+    }],
+    reviews: {
+        type: Number,
+        default: 0
+    },
+    deletedAt: {
+        type: Date,
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    releasedAt: {
+        type: Date,
+        required: true,
+
+    }
+}, { timestamps: true });
+module.exports = mongoose.model('bookModel', bookSchema)
+
+
+
---
+++ src/route/route.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controller/userController');
+const bookController = require('../controller/bookController');
+const reviewController = require('../controller/reviewController');
+const middleware=require("../middleware/middleware")
+
+//====================================================APIs=================================================================================
+
+//---------------- USER API'S ----------------------
+router.post('/register', userController.createUser)
+router.post('/login', userController.userLogin)
+router.get('/getUser', userController.getUser)
+
+//---------------- BOOK API'S ----------------------
+router.post("/books", middleware.authenticate, middleware.authorization, bookController.createBook)
+router.get("/books",
+//  middleware.authenticate, 
+ bookController.getBooksData)
+router.get("/books/:bookId", middleware.authenticate, bookController.getBooksDataById)
+router.put("/books/:bookId", middleware.authenticate, middleware.authorization, bookController.updatedBook)
+router.delete("/books/:bookId", middleware.authenticate, middleware.authorization, bookController.deleteBookId)
+
+//---------------- REVIEW API'S ----------------------
+router.post("/books/:bookId/review",reviewController.createReview)
+router.put("/books/:bookId/review/:reviewId",reviewController.updatedReview)
+router.delete("/books/:bookId/review/:reviewId",reviewController.deleteReview)
+
+
+module.exports = router;
